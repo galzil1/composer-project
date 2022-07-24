7b54f76c93aaf14f45b1e066e2d23cbae60b6d4f
Gal Zilberman <galzi@jfrog.com>
2022-07-24 09:12:55 +0300

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'galzil1/my-project',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'galzil1/my-project' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roots/sage' => array(
+            'pretty_version' => 'v10.2.0',
+            'version' => '10.2.0.0',
+            'reference' => '46babd52dfa4d3af5bfa89d2c5c6b195630051c1',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../roots/sage',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
